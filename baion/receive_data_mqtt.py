1d2f860546c17cfb080be6fb5ddc1faaf1b18c7d
Le Van Duy <levanduy093@gmail.com>
2025-11-08 14:29:14 +0700

    done mqtt

--- baion/receive_data_mqtt.py
+++ baion/receive_data_mqtt.py
@@ -3,9 +3,13 @@
 # clientID    = "IRU6PSACOwQPHy4PKiczCiI"
 # password    = "gHzqnX35vjOPS0jeNUVtBdfV"
 
+# Biến lưu tạm để ghép cặp
+current_temp = None
+current_humi = None
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with Result code {}".format(rc))
-    # subscribes to updates to channel fields (Field3 & Field4)
+    # Subscribe field3 & field4
     client.subscribe("channels/%s/subscribe/fields/field3" % (channel_ID))
     client.subscribe("channels/%s/subscribe/fields/field4" % (channel_ID))
 
@@ -13,8 +17,25 @@ def on_disconnect(client, userdata, rc):
     print("Disconnected From Broker")
 
 def on_message(client, userdata, message):
-    print(message.payload.decode())
-    #print(message.topic)
+    global current_temp, current_humi
+
+    value = message.payload.decode().strip()
+    topic = message.topic
+
+    # Lưu giá trị đúng field
+    if topic.endswith("field3"):
+        current_temp = value
+    elif topic.endswith("field4"):
+        current_humi = value
+
+    # In ra khi có đủ cả nhiệt độ & độ ẩm
+    if current_temp is not None and current_humi is not None:
+        print(f"Nhiệt độ: {current_temp} °C | Độ ẩm: {current_humi} %")
+
+        # Reset để chờ cặp tiếp theo
+        current_temp = None
+        current_humi = None
+
 
 import paho.mqtt.client as mqtt
 
